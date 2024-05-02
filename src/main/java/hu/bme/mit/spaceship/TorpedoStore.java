2ccecad38de966cea4389add9956c7a7524cd00b
szaszdora <127404715+szaszdora@users.noreply.github.com>
2024-05-03 00:28:01 +0200

    deleteemptylines

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -11,9 +11,7 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-
   private int torpedoCount = 0;
-
   private Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
@@ -34,9 +32,7 @@ public class TorpedoStore {
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
     }
-
     boolean success = false;
-
     // simulate random overheating of the launcher bay which prevents firing
     double r = generator.nextDouble();
 
